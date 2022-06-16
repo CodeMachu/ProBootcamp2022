5cc0de12e5bf0bdf32ab5a468fab3f45017abe6f
Machu <codemachu@gmail.com>
2022-06-16 09:51:18 -0400

    Day 8 Updates

---
+++ Day_8_Keyword_Arguments/main.py
@@ -0,0 +1,21 @@
+# Positional Arguments vs Keyword Arguments
+
+a = 3
+b = 9
+c = 27
+
+# Values must be passed in when called
+def positional_arg(a, b, c):
+    print("This is a positional argument")
+    print(f"Values:\na:{a}\nb:{b}\nc:{c}")
+
+# Values are predefined before the function is called
+def keyword_arg(a=1,b=2,c=3):
+    print("This is a keyword argument")
+    print(f"a:{a}\nb:{b}\nc:{c}")
+
+# Call the functions
+positional_arg(a, b, c)
+
+# This call does not require parameters
+keyword_arg()
